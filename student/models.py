8f1c486b98b5e75aae7fdf70b3e3ad172d0d05c6
MansiAhir <mansiahir2003@gmail.com>
2026-02-04 12:08:20 +0530

    model creation

--- student/models.py
+++ student/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    studentName = models.CharField(max_length=100)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+    class Meta:
+        db_table = "student"
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+       
+    class Meta:
+        db_table = "product"
+
+class AddToCart(models.Model):
+    cartDetails = models.CharField(max_length=100)
+    Quantity = models.IntegerField()
+    Price = models.IntegerField()
+    Date = models.DateField()
+
+    class Meta:
+        db_table = "addtocart"
